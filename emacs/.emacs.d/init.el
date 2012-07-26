ccc978349e784bfb5b24f80ecfb9d1fded15ccad
Yusuke Odate <viz3.dev@gmail.com>
2012-07-26 12:13:07 +0900

    update

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -8,11 +8,26 @@
 (column-number-mode 1)
 (auto-fill-mode -1)
 
-(let ((default-directory (expand-file-name "~/.emacs.d/lisp")))
- (add-to-list 'load-path default-directory)
- (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-     (normal-top-level-add-subdirs-to-load-path)))
-;(add-to-list 'load-path "~/.emacs.d/site-lisp")
+;; load-path setting
+;; see https://bitbucket.org/sakito/dot.emacs.d/wiki/Home
+
+;; Emacs 設定ディレクトリを設定。Emacs 22以下用
+;; Emacs 23.1 以上では user-emacs-directory 変数が用意されているのでそれを利用
+(unless (boundp 'user-emacs-directory)
+  (defvar user-emacs-directory (expand-file-name "~/.emacs.d/")))
+
+;; 引数を load-path へ追加
+;; normal-top-level-add-subdirs-to-load-path はディレクトリ中の中で
+;; [A-Za-z] で開始する物だけ追加するので、追加したくない物は . や _ を先頭に付与しておけばロードしない
+;; dolist は Emacs 21 から標準関数なので積極的に利用して良い
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths)
+      (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+(add-to-load-path "lisp" "site-lisp")
 
 ;; skk
 (require 'skk-setup)
@@ -67,15 +82,20 @@
 (add-hook 'c++-mode-hook '(lambda () (gtags-mode 1)))
 (add-hook 'php-mode-hook '(lambda () (gtags-mode 1)))
 
-;; cocoa-emacs
-(cond ((eq window-system 'ns)
-       (tool-bar-mode -1)
-       (scroll-bar-mode -1)
-       ;; font
-       (create-fontset-from-ascii-font "Menlo-14:weight=normal:slant=normal" nil "menlokakugo")
-       (set-fontset-font "fontset-menlokakugo"
-			 'unicode
-			 (font-spec :family "Hiragino Kaku Gothic ProN" :size 16)
-			 nil
-			 'append)
-       (add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))))
+;; environment specific preferences
+(cond
+ ;; cocoa-emacs
+ ((eq window-system 'ns)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  ;; font
+  (create-fontset-from-ascii-font "Menlo-14:weight=normal:slant=normal" nil "menlokakugo")
+  (set-fontset-font "fontset-menlokakugo"
+		    'unicode
+		    (font-spec :family "Hiragino Kaku Gothic ProN" :size 16)
+		    nil
+		    'append)
+  (add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))
+  ;; gtags
+  (setq gtags-global-command "/usr/local/bin/global")
+  ))
