2a418d6bfe47b4703ea5d2f6a8a230f11cb9a543
Yusuke Odate <yusuke.odate@gree.net>
2013-02-01 12:24:00 +0900

    change font fize from 16 to 14 on mac.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -169,10 +169,10 @@
   (tool-bar-mode -1)
   (scroll-bar-mode -1)
   ;; font
-  (create-fontset-from-ascii-font "Menlo-14:weight=normal:slant=normal" nil "menlokakugo")
+  (create-fontset-from-ascii-font "Menlo-12:weight=normal:slant=normal" nil "menlokakugo")
   (set-fontset-font "fontset-menlokakugo"
                     'unicode
-                    (font-spec :family "Hiragino Kaku Gothic ProN" :size 16)
+                    (font-spec :family "Hiragino Kaku Gothic ProN" :size 14)
                     nil
                     'append)
   (add-to-list 'default-frame-alist '(font . "fontset-menlokakugo"))))
